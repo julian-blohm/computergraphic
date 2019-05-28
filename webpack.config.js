237bd2713d974a621969b49fdba30aaaa0da3a4a
blueprint314 <julianblohm@web.de>
2019-05-28 15:28:16 +0200

    aufbau der Grundstruktur

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: ['babel-polyfill', './src/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  plugins: [new HtmlWebpackPlugin({
+    title: 'Computergrafik-Projekt',
+    template: 'src/index.html'
+  })],
+  module: {
+    rules: [
+        
+    ]
+  }
+};
