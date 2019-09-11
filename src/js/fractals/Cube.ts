d66ac301e88cabf17c06d5521213d174918c5908
Karim Ould Mahieddine <karim_om@me.com>
2019-09-11 14:03:33 +0200

    menu and cube

--- src/js/fractals/Cube.ts
+++ src/js/fractals/Cube.ts
@@ -5,7 +5,14 @@ export default class Cube extends Fractral {
   //Test Cube
   public init(): void {
     // Create a Cube Mesh with basic material
-    var geometry = new _three.BoxGeometry(1, 1, 1)
+    var geometry = new _three.BoxGeometry(
+      2.5,
+      2.5,
+      2.5,
+      Math.pow(this.getLevel, 2),
+      Math.pow(this.getLevel, 2),
+      Math.pow(this.getLevel, 2),
+    )
     var material = new _three.MeshBasicMaterial({ color: this.getColor, wireframe: true, wireframeLinewidth: 1 })
     var cube = new _three.Mesh(geometry, material)
     const scene = this.getMainScene.getScene
